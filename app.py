e8f7f496cc645e661a0b940389cb66f5bc0c6941
nikolpetkova10 <petkovaniki123@gmail.com>
2026-02-04 13:39:17 +0200

    Change input type for capital city question

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 st.title("Geography")
-answer = st.number_input("Коя е столицата на България?", step=1)
+answer = st.text_input("Коя е столицата на България?", step=1)
 if st.button("Провери"):
   if answer == София:
     st.success("Вярно! Браво")
