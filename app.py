9c8129a4dac999e7322e1cc50f16ce9114ee7a82
nikolpetkova10 <petkovaniki123@gmail.com>
2026-02-04 13:47:11 +0200

    Refactor input variables for capital questions

--- app.py
+++ app.py
@@ -1,36 +1,36 @@
 import streamlit as st
 st.title("Geography")
-answer = st.text_input("Коя е столицата на България?")
+answer1 = st.text_input("Коя е столицата на България?")
 if st.button("Провери"):
-  if answer == "София":
+  if answer1 == "София":
     st.success("Вярно! Браво")
   else:
     st.error("Грешно. Опитай пак ")
 
-answer = st.text_input("Коя е столицата на Япония?")
+answer2 = st.text_input("Коя е столицата на Япония?")
 if st.button("Провери"):
-  if answer == "Токио":
+  if answer2 == "Токио":
     st.success("Вярно! Браво")
   else:
     st.error("Грешно. Опитай пак ")
 
-answer = st.text_input("Коя е столицата на Белгия?")
+answer3 = st.text_input("Коя е столицата на Белгия?")
 if st.button("Провери"):
-  if answer == "Брюксел":
+  if answer3 == "Брюксел":
     st.success("Вярно! Браво")
   else:
     st.error("Грешно. Опитай пак ")
 
-answer = st.text_input("Коя е столицата на Германия?")
+answer4 = st.text_input("Коя е столицата на Германия?")
 if st.button("Провери"):
-  if answer == "Берлин":
+  if answer4 == "Берлин":
     st.success("Вярно! Браво")
   else:
     st.error("Грешно. Опитай пак ")
     
-answer = st.text_input("Коя е столицата на Гърция?")
+answer5 = st.text_input("Коя е столицата на Гърция?")
 if st.button("Провери"):
-  if answer == "Атина":
+  if answer5 == "Атина":
     st.success("Вярно! Браво")
   else:
     st.error("Грешно. Опитай пак ")
