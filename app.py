238df43bce054d9e5b6a78d49060c90b5c66d430
nikolpetkova10 <petkovaniki123@gmail.com>
2026-02-04 13:45:51 +0200

    Add capital city quiz questions to app.py

--- app.py
+++ app.py
@@ -7,4 +7,30 @@ if st.button("Провери"):
   else:
     st.error("Грешно. Опитай пак ")
 
+answer = st.text_input("Коя е столицата на Япония?")
+if st.button("Провери"):
+  if answer == "Токио":
+    st.success("Вярно! Браво")
+  else:
+    st.error("Грешно. Опитай пак ")
 
+answer = st.text_input("Коя е столицата на Белгия?")
+if st.button("Провери"):
+  if answer == "Брюксел":
+    st.success("Вярно! Браво")
+  else:
+    st.error("Грешно. Опитай пак ")
+
+answer = st.text_input("Коя е столицата на Германия?")
+if st.button("Провери"):
+  if answer == "Берлин":
+    st.success("Вярно! Браво")
+  else:
+    st.error("Грешно. Опитай пак ")
+    
+answer = st.text_input("Коя е столицата на Гърция?")
+if st.button("Провери"):
+  if answer == "Атина":
+    st.success("Вярно! Браво")
+  else:
+    st.error("Грешно. Опитай пак ")
